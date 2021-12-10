56e0f17f24003216a8f95b315655badd54b7cb39
Marcos Corona <mcorona.avalos02@gmail.com>
2021-12-10 15:03:19 -0300

    feat: improve telescope config

--- config/nvim/lua/ryuphi/telescope.lua
+++ config/nvim/lua/ryuphi/telescope.lua
@@ -1,6 +1,23 @@
 local telescope = require('telescope')
 
-telescope.setup {} 
+telescope.setup {
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'smart_case',
+        }
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+        },
+        live_grep = {
+            path_display = { 'shorten' }
+        },
+    }
+} 
 
 
 -- load fzf extension
